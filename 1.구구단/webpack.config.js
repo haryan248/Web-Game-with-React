09376ac884adacfce834c2fcec6f6a5c734497a7
haryan248 <haryan96@naver.com>
2022-11-28 15:06:36 +0900

    리엑트 구구단 강의

---
+++ 1.구구단/webpack.config.js
@@ -0,0 +1,43 @@
+const path = require("path");
+const webpack = require("webpack");
+
+module.exports = {
+  mode: "development",
+  devtool: "inline-source-map", // hidden-source-map
+  resolve: {
+    extensions: [".jsx", ".js"],
+  },
+  entry: {
+    // 입력
+    app: "./client",
+  },
+  module: {
+    rules: [
+      {
+        // js, jsx에 바벨 로더 옵션 적용
+        test: /\.jsx?$/,
+        loader: "babel-loader",
+        options: {
+          presets: [
+            [
+              "@babel/preset-env",
+              {
+                targets: {
+                  browsers: ["> 1% in KR"], // browserslist
+                },
+                debug: true,
+              },
+            ],
+            "@babel/preset-react",
+          ],
+          plugins: [],
+        },
+      },
+    ],
+  },
+  output: {
+    // 출력
+    filename: "app.js",
+    path: path.join(__dirname, "dist"),
+  },
+};
