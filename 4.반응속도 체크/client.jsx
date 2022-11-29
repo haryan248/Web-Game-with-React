72e1f1e22ac1bcb97f9877ba7642daf640b88e4f
haryan248 <haryan96@naver.com>
2022-11-29 16:13:26 +0900

    가위바위보 리엑트 18 적용

--- 4.반응속도 체크/client.jsx
+++ 4.반응속도 체크/client.jsx
@@ -3,4 +3,5 @@ import ReactDOM from "react-dom";
 
 import ResponseCheck from "./ResponseCheck.jsx";
 
-ReactDOM.render(<ResponseCheck />, document.querySelector("#root"));
+// ReactDOM.render(<ResponseCheck />, document.querySelector("#root"));
+ReactDOM.createRoot(document.querySelector("#root")).render(<ResponseCheck />);
---
+++ 5.가위바위보/client.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+import RSP from "./RSP";
+
+// ReactDOM.render(<RSP />, document.querySelector("#root"));
+ReactDOM.createRoot(document.querySelector("#root")).render(<RSP />);
