c69e707d613fe8fb933009734e6e456f1441f967
haryan248 <haryan96@naver.com>
2022-12-07 15:00:04 +0900

    react-router 세팅

---
+++ react-router/GameMatcher.tsx
@@ -0,0 +1,22 @@
+import * as React from "react";
+import NumberBaseball from "../3.숫자야구(ts)/NumberBaseballClass";
+import RSP from "../5.가위바위보(ts)/RSPClass";
+import Lotto from "../6.로또(ts)/LottoClass";
+import { useLocation, useNavigate, Routes, Route } from "react-router";
+
+const GameMatcher = () => {
+  const location = useLocation();
+  const navigate = useNavigate();
+  let urlSearchParams = new URLSearchParams(location.search.slice(1));
+  console.log(urlSearchParams.get("page"));
+  return (
+    <Routes>
+      <Route path="number-baseball" element={<NumberBaseball />} />
+      <Route path="rock-scissors-paper" element={<RSP />} />
+      <Route path="lotto-generator" element={<Lotto />} />
+      <Route path="*" element={<div>일치하는 게임이 없습니다.</div>} />
+    </Routes>
+  );
+};
+
+export default GameMatcher;
---
+++ react-router/client.tsx
@@ -0,0 +1,6 @@
+import * as React from "react";
+import * as ReactDOM from "react-dom/client";
+
+import Games from "./Games";
+
+ReactDOM.createRoot(document.querySelector("#root")!).render(<Games />);
